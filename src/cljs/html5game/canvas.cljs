40c86750b84a28aa1cbd8609625e59cb08e8b185
Stephen Cagle <samedhi@gmail.com>
2015-10-24 17:48:21 -0700

    Now the bubbles are rotated randomly when  drawn

--- src/cljs/html5game/canvas.cljs
+++ src/cljs/html5game/canvas.cljs
@@ -16,10 +16,16 @@
       (.clearRect context 0 0 (.-width canvas) (.-height canvas))))
 
 (defn draw-bubble! [bubble-index bubble-state pixel-x pixel-y]
-  (let [b config/SPRITE_SHEET_CLIP_BOUNDARY]
-    (.drawImage (get-context)
+  (let [b config/SPRITE_SHEET_CLIP_BOUNDARY
+        c (get-context)
+        r (* js/Math.PI (rand) 2)]
+    (.translate c pixel-x pixel-y)
+    (.rotate c r)
+    (.drawImage c
                 config/SPRITE_SHEET
-                (* b bubble-index) (* b bubble-state)
+                (* b bubble-state) (* b bubble-index)
                 b b
-                pixel-x pixel-y
-                b b)))
+                (/ b -2) (/ b -2)
+                b b)
+    (.rotate c (* -1 r))
+    (.translate c (* pixel-x -1) (* pixel-y -1))))
